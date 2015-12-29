a0aec1de6851a5f8b6463fb1f0d511e9556c61a7
Dmitry Risics <dmitrijs.risics@vce.com>
2015-12-29 15:46:24 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra/base'
+require 'tilt/haml'
+require 'tilt/sass'
+require './helpers'
+
+class App < Sinatra::Base
+
+  helpers Helpers
+
+  set :views, :scss => 'views/scss', :default => 'views'
+
+  configure :development do
+    enable :dump_errors, :logging
+    set :server, 'puma'
+    set :bind, '0.0.0.0'
+    set :port, 3000
+  end
+
+  get '/css/:file.css' do
+    halt 404 unless File.exist?("views/scss/#{params[:file]}.scss")
+    time = File.stat("views/scss/#{params[:file]}.scss").ctime
+    last_modified(time)
+    scss params[:file].intern
+  end
+
+  get '/*' do
+    haml :app
+  end
+
+  run! if app_file == $0
+end
